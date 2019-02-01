5749b8af265593b8d52295e992ec5ca75544fe43
zengbin <zengbin@arashivision.com>
2019-02-01 18:09:05 +0800

    first commit

---
+++ hello_world_test.cc
@@ -0,0 +1,5 @@
+#include <ree/unittest.h>
+
+R_TEST_F(Sample, HelloWorld) {
+    R_ASSERT_EQ(1 + 1, 2);
+}
---
+++ main.cc
@@ -0,0 +1,7 @@
+#include <ree/unittest.h>
+
+int main(int argc, char const *argv[]) {
+    R_TEST_INIT(argc, argv);
+    R_RUN_TESTS();
+    return 0;
+}
